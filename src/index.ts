f26b9599266cb0eca181345527f5b3ae7269b90e
Stella <live.hylia@outlook.com>
2023-06-18 00:23:57 -0400

    optional username

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/:username", async (req, res) => {
+app.get("/:username?", async (req, res) => {
     const username = req.params.username;
 
     if (!username) return res.redirect("https://goddessanime.com")
